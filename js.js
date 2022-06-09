66ce3345ef047cabb8d49b7d04560b6a5128c2d2
NadjaNS <100866219+NadjaNS@users.noreply.github.com>
2022-06-09 14:22:33 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,31 +1,25 @@
 document.onclick = () => {
 
 
-const vreme = new Date()
+const vreme = new Date();
 document.getElementById(`vreme`).innerHTML=vreme;
 
 
-
-
-
-
-
-
-
+// regularni  izrazi
 document.getElementById(`btn`).addEventListener(`click`,function(e){
-  e.preventDefault()
+  e.preventDefault();
   let brojKnjizice = document.getElementById(`unos`).value;
-  let pattern = /^[0-9]{11}$/
-  let regError = document.querySelector(".error")
+  let pattern = /^[0-9]{11}$/;
+  let regError = document.querySelector(".error");
   if(pattern.test(brojKnjizice)){  
-    regError.innerHTML="Format je ispravan "
+    regError.innerHTML=" ";
   }else{
-    regError.innerHTML="Broj knjizice nije u odgovarajucem formatu."
-    document.getElementById(`ispis`).innerHTML =" "
+    regError.innerHTML="Broj knjizice nije u odgovarajucem formatu.";
+    
 }
 });
 
-document.getElementById(`btn`).addEventListener(`click`,filterChanged)
+document.getElementById(`btn`).addEventListener(`click`,filterChanged);
 dohvatiPodatke("pacijenti", ispisiPacijente);
 
 function dohvatiPodatke(file, callback) {
@@ -33,7 +27,7 @@ function dohvatiPodatke(file, callback) {
   zahtev.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       callback(JSON.parse(this.responseText)); 
-    }
+    };
     if (this.status >= 400) {
       
       var greska = new Error("Request failed:" + zahtev.statusText);
@@ -46,10 +40,10 @@ function dohvatiPodatke(file, callback) {
 
 
 
-
+// iscitavanje podataka
 function ispisiPacijente(data){
   data = searchFilter(data);
-  let html= ``
+  let html= ``;
     
     
     data.forEach(element => {
@@ -58,13 +52,13 @@ function ispisiPacijente(data){
       <ul class="podaci">Ime i prezime pacijenta: <li class="pacijent">${element.ime}</li></ul>
       <ul class="podaci">LBO pacijenta: <li class="pacijent">${element.lbo}</li></ul>
       <ul class="podaci">Datum rodjenja pacijenta: <li class="pacijent">${element.datum_rodjenja}</li></ul>
-      <ul class="podaci">Propisana terapija: <li class="pacijent">${element.lekovi}</li></ul>
-      <ul>Datum Propisane terapije:</ul>`
+      <ul class="podaci">Propisana terapija: <li class="pacijent" id= "terapija">${element.lekovi}</li></ul>
+      <ul class="podaci">Datum Propisane terapije:</ul>`;
       
     });
-      document.getElementById(`ispis`).innerHTML = html
+      document.getElementById(`ispis`).innerHTML = html;
       
-    };
+    }
 
 function filterChanged(){
      dohvatiPodatke("pacijenti", ispisiPacijente); 
@@ -75,33 +69,19 @@ function searchFilter(data) {
   let search = document.getElementById("unos").value;
   if (search) {
     return data.filter((element) => element.brk.indexOf(search) != -1);
-    
-  }
-  
-  
-  }
-    
-    
-    
-  
-    
-    
-    
-    
-    
+    }
+}
 
 
+    
+ let broj=document.getElementById(`unos`).value;
  
- 
-
-
-
-
-
- 
-
-
-  }
+ localStorage.setItem("Broj knjizice",broj);
+    
+    
+    
+    
+};
 
 
 
